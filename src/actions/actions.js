48ee006eaad1e49a95c4148989cc4d28055c4a2d
주현정 <61533589+joooo24@users.noreply.github.com>
2024-05-17 15:45:06 +0900

    chore: actions.js 주석 추가

--- src/actions/actions.js
+++ src/actions/actions.js
@@ -1,26 +1,30 @@
 // actions.js
 
+// 액션 타입 상수 정의
 export const ADD_TASK = "ADD_TASK";
 export const UPDATE_TASK = "UPDATE_TASK";
 export const DELETE_TASK = "DELETE_TASK";
 
+// 할일 추가 액션 생성자 함수
 export const addTask = (task) => {
   return {
     type: ADD_TASK,
-    payload: task,
+    payload: task, // 새로운 할일 데이터
   };
 };
 
+// 할일 업데이트 액션 생성자 함수
 export const updateTask = (taskId, isChecked) => {
   return {
     type: UPDATE_TASK,
-    payload: { taskId, isChecked },
+    payload: { taskId, isChecked }, // 할일 아이디와 체크 여부
   };
 };
 
+// 할일 삭제 액션 생성자 함수
 export const deleteTask = (taskId) => {
   return {
     type: DELETE_TASK,
-    payload: taskId,
+    payload: taskId, // 삭제할 할일 아이디
   };
 };
