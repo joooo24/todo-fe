f16c37096f35f425425c7c66c04003fb2c1a19bf
주현정 <61533589+joooo24@users.noreply.github.com>
2024-05-17 16:31:52 +0900

    feat: useDispatch, useSelector 사용하여 데이터 핸들링

--- src/components/TodoBoard.js
+++ src/components/TodoBoard.js
@@ -1,13 +1,20 @@
 import React from "react";
+import { useSelector } from "react-redux";
 import TodoItem from "./TodoItem";
 
-const TodoBoard = ({ tasks, updateTask, deleteTask }) => {
-  return (
-    <div>
-      <h2>Todo List</h2>
-      {tasks.length > 0 ? <TodoItem tasksData={tasks} updateTask={updateTask} deleteTask={deleteTask} /> : <h2>할 일 목록이 없습니다.</h2>}
-    </div>
-  );
+const TodoBoard = () => {
+    const tasks = useSelector((state) => state.tasks);
+
+    return (
+        <div>
+            <h2>Todo List</h2>
+            {tasks.length > 0 ? (
+                <TodoItem tasksData={tasks} />
+            ) : (
+                <h2>할 일 목록이 없습니다.</h2>
+            )}
+        </div>
+    );
 };
 
 export default TodoBoard;
