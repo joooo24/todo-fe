3b5903968eee8466fc687e6c3b67e911e2bbf0c2
주현정 <61533589+joooo24@users.noreply.github.com>
2024-05-21 17:52:41 +0900

    chore: 뎁스 정리

--- src/components/TodoBoard.js
+++ src/components/TodoBoard.js
@@ -2,12 +2,12 @@ import React from "react";
 import TodoItem from "./TodoItem";
 
 const TodoBoard = ({ tasks, updateTask, deleteTask }) => {
-  return (
-    <div>
-      <h2>Todo List</h2>
-      {tasks.length > 0 ? <TodoItem tasksData={tasks} updateTask={updateTask} deleteTask={deleteTask} /> : <h2>할 일 목록이 없습니다.</h2>}
-    </div>
-  );
+    return (
+        <div>
+            <h2>Todo List</h2>
+            {tasks.length > 0 ? <TodoItem tasksData={tasks} updateTask={updateTask} deleteTask={deleteTask} /> : <h2>할 일 목록이 없습니다.</h2>}
+        </div>
+    );
 };
 
 export default TodoBoard;
