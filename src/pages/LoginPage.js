a63ae824fea876201e3c863077df1196041fb0d9
주현정 <61533589+joooo24@users.noreply.github.com>
2024-05-22 10:04:23 +0900

    chore: css 수정

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -6,8 +6,8 @@ import { Link } from "react-router-dom";
 
 const LoginPage = () => {
     return (
-        <div className="display-center">
-            <Form className="login-box">
+        <div className="form-container">
+            <Form className="form-box">
                 <h1>로그인</h1>
                 <Form.Group className="mb-3" controlId="formBasicEmail">
                     <Form.Label>Email address</Form.Label>
--- src/pages/RegisterPage.js
+++ src/pages/RegisterPage.js
@@ -4,26 +4,26 @@ import Form from "react-bootstrap/Form";
 
 const RegisterPage = () => {
     return (
-        <div className="display-center">
-            <Form className="login-box">
+        <div className="form-container">
+            <Form className="form-box">
                 <h1>회원가입</h1>
                 <Form.Group className="mb-3" controlId="formName">
-                    <Form.Label>Name</Form.Label>
+                    <Form.Label>이름</Form.Label>
                     <Form.Control type="string" placeholder="Name" />
                 </Form.Group>
 
                 <Form.Group className="mb-3" controlId="formBasicEmail">
-                    <Form.Label>Email address</Form.Label>
+                    <Form.Label>이메일</Form.Label>
                     <Form.Control type="email" placeholder="Enter email" />
                 </Form.Group>
 
                 <Form.Group className="mb-3" controlId="formBasicPassword">
-                    <Form.Label>Password</Form.Label>
+                    <Form.Label>비밀번호</Form.Label>
                     <Form.Control type="password" placeholder="Password" />
                 </Form.Group>
 
                 <Form.Group className="mb-3" controlId="formBasicPassword">
-                    <Form.Label>re-enter the password</Form.Label>
+                    <Form.Label>비밀번호 재입력</Form.Label>
                     <Form.Control type="password" placeholder="re-enter the password" />
                 </Form.Group>
 
