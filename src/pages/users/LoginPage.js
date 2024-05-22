982f2012b6ced1729ba456d507b8785a594bb87d
주현정 <61533589+joooo24@users.noreply.github.com>
2024-05-22 14:37:47 +0900

    chore: 입력 값 저장

--- src/pages/users/LoginPage.js
+++ src/pages/users/LoginPage.js
@@ -1,27 +1,36 @@
-import React from "react";
-import Button from "react-bootstrap/Button";
+import React, { useState } from "react";
 import Form from "react-bootstrap/Form";
 import { Link } from "react-router-dom";
 import "./users.scss";
 
 const LoginPage = () => {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
     return (
         <div className="form-container">
             <Form className="form-box">
                 <h1>로그인</h1>
                 <Form.Group className="mb-3" controlId="formBasicEmail">
                     <Form.Label>이메일</Form.Label>
-                    <Form.Control type="email" placeholder="Enter email" />
+                    <Form.Control
+                        type="email"
+                        placeholder="Enter email"
+                        value={email}
+                        onChange={(event) => { setEmail(event.target.value) }}
+                    />
                 </Form.Group>
 
                 <Form.Group className="mb-3" controlId="formBasicPassword">
                     <Form.Label>비밀번호</Form.Label>
-                    <Form.Control type="password" placeholder="Password" />
+                    <Form.Control
+                        type="password"
+                        placeholder="Password"
+                        value={password}
+                        onChange={(event) => { setPassword(event.target.value) }}
+                    />
                 </Form.Group>
                 <div className="button-box">
-                    <Button type="submit" className="button-primary">
-                        Login
-                    </Button>
+                    <button type="submit">로그인</button>
                     <span>
                         계정이 없다면? <Link to="/register">회원가입 하기</Link>
                     </span>
--- src/pages/users/RegisterPage.js
+++ src/pages/users/RegisterPage.js
@@ -1,30 +1,54 @@
-import React from "react";
+import React, { useState } from "react";
 import Form from "react-bootstrap/Form";
 import "./users.scss";
 
 const RegisterPage = () => {
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [rePassword, setRePassword] = useState("");
     return (
         <div className="form-container">
             <Form className="form-box">
                 <h1>회원가입</h1>
                 <Form.Group className="mb-3" controlId="formName">
                     <Form.Label>이름</Form.Label>
-                    <Form.Control type="string" placeholder="Name" />
+                    <Form.Control
+                        type="string"
+                        placeholder="Name"
+                        value={name}
+                        onChange={(event) => { setName(event.target.value) }}
+                    />
                 </Form.Group>
 
                 <Form.Group className="mb-3" controlId="formBasicEmail">
                     <Form.Label>이메일</Form.Label>
-                    <Form.Control type="email" placeholder="Enter email" />
+                    <Form.Control
+                        type="email"
+                        placeholder="Enter email"
+                        value={email}
+                        onChange={(event) => { setEmail(event.target.value) }}
+                    />
                 </Form.Group>
 
                 <Form.Group className="mb-3" controlId="formBasicPassword">
                     <Form.Label>비밀번호</Form.Label>
-                    <Form.Control type="password" placeholder="Password" />
+                    <Form.Control
+                        type="password"
+                        placeholder="Password"
+                        value={password}
+                        onChange={(event) => { setPassword(event.target.value) }}
+                    />
                 </Form.Group>
 
                 <Form.Group className="mb-3" controlId="formBasicPassword">
                     <Form.Label>비밀번호 재입력</Form.Label>
-                    <Form.Control type="password" placeholder="re-enter the password" />
+                    <Form.Control
+                        type="password"
+                        placeholder="re-enter the password"
+                        value={rePassword}
+                        onChange={(event) => { setRePassword(event.target.value) }}
+                    />
                 </Form.Group>
 
                 <button type="submit">회원가입</button>
